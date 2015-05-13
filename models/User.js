f7ef20048300836e06094eba174c1831848ed9be
Nick Eagan <neagan26@gmail.com>
2015-05-12 18:23:20 -0700

    add user schema

---
+++ models/User.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var eat = require('eat');
+
+var userSchema = mongoose.Schema({
+  username: String,
+  basic: {
+    email: { type: String, unique: true},
+    password: String
+  }
+});
+
+userSchema.methods.generateHash = function(password) {
+  bcrypt.genSalt(8, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg: 'Internal Server Error'});
+      }
+      return hash; // ?
+    });
+  });
+};
+
+userSchema.methods.checkPassword = function(password) {
+  bcrypt.compare(password, this.basic.password, function(err, res) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({msg: 'Internal Server Error'})
+    }
+    return res; // ?
+  })
+};
+
+userSchema.methods.generateToken = function(secret, callback) {
+  eat.encode({id: this._id}, secret, callback);
+}
