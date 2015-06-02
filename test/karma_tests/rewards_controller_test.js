8eea11d1c6ae14f7c85116b2aa6f0317838a7bbc
Nick Eagan <neagan26@gmail.com>
2015-06-02 16:23:53 -0700

    refactor put test

--- test/karma_tests/rewards_controller_test.js
+++ test/karma_tests/rewards_controller_test.js
@@ -101,13 +101,10 @@ describe('rewards controller', function() {
       $httpBackend.expectPUT('/api/rewards/5').respond(200, {msg: 'update successful'});
       expect($scope.rewards[0].level).toBe('diamond');
       expect($scope.rewards[0].points).toBe(4000);
-      reward.level = 'platinum';
-      reward.points = 3999;
       $scope.updateReward(reward);
       $httpBackend.flush();
 
-      expect($scope.rewards[0].level).toBe('platinum');
-      expect($scope.rewards[0].points).toBe(3999);
+      expect($scope.errors.length).toBe(0);
     });
 
   });
